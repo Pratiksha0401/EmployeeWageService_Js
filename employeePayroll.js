2f510846a7def057f1dc33b53f3e99ce39e64575
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-01 23:07:12 +0530

    Added : code to check id and salary are non zero and gender is M or F only

--- employeePayroll.js
+++ employeePayroll.js
@@ -1,10 +1,8 @@
-
+//creating given class
 class EmployeePayrollData {
-     id;
-     salary;
-     gender;
+    
      startDate
-
+    //creating parameterized constructor with given parameters using var args
     constructor(...params) {
      this.id = params[0];
      this.name = params[1];
@@ -20,6 +18,29 @@ class EmployeePayrollData {
      this._name = name;
      else throw 'Invalid Name!';
    }
+   get id() {return this._id;}
+   set id(id) {
+     let idRegex = RegExp('^[1-9][0-9]*$');
+     if(idRegex.test(id))
+     this._id = id;
+     else throw 'Invalid id!';
+   }
+   get salary() {return this._salary;}
+   set salary(salary) {
+     let salaryRegex = RegExp('^[0-9]*[1-9][0-9]*(\.[0-9]+)?|[0]+\.[0-9]*[1-9][0-9]*$');
+     if(salaryRegex.test(salary))
+          this._salary = salary;
+     else throw 'Invalid salary!';
+   }
+
+   get gender(){return this._gender}
+   set gender(gender){
+     let genderRegex = RegExp('^[F M]{1}$')
+     if(genderRegex.test(gender))
+          this._gender = gender;
+     else throw 'Invalid gender'     
+   }
+ 
  
    toString() {
       return "id = "+ this.id +", name = "+this.name+", salary = "+this.salary +
@@ -28,12 +49,57 @@ class EmployeePayrollData {
  }
    let employeePayrolldata = new EmployeePayrollData(1, "Mark", 60000,"M","2021-05-22");
    console.log("Payroll Data: "+employeePayrolldata.toString());
-   //using try catch block to check name validation
+   //using try catch block to check name validation for incorrect name
    try{
    employeePayrolldata.name = "james";
    console.log(employeePayrolldata.toString())
    }catch(e){
      console.error(e);
    }
-   let newemployeePayrolldata = new EmployeePayrollData(1, "Terisa", 50000 ,"F", "2020-05-31");
+   //check for id validation with invalid input
+   try{
+     employeePayrolldata.id = "-1";
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+   //check for id validation with valid input
+   try{
+     employeePayrolldata.id = 2;
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+   //check for salary validation with invalid input shows error
+   try{
+     employeePayrolldata.salary = "-23454";
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+   //check for salary validation with valid input 
+   try{
+     employeePayrolldata.salary = 40000;
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+
+   //check for gender validation with invalid input 
+   try{
+     employeePayrolldata.gender = 'g';
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+
+   //check for gender validation with valid input 
+   try{
+     employeePayrolldata.gender = 'M';
+     console.log(employeePayrolldata.toString())
+   }catch(e){
+     console.error(e);
+   }
+
+   let newemployeePayrolldata = new EmployeePayrollData(1, "Nishad", 50000 ,"M", "2020-05-31");
    console.log("New Payroll Data: "+newemployeePayrolldata.toString());
